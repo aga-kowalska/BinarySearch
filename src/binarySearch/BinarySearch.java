5732e793cbb9d269a745939b5b9df868359f868d
aga-kowalska <91847436+aga-kowalska@users.noreply.github.com>
2022-10-03 20:46:06 +0100

    binary search implemented

--- src/binarySearch/BinarySearch.java
+++ src/binarySearch/BinarySearch.java
@@ -3,8 +3,27 @@ package binarySearch;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] array = {1, 2, 3, 4, 5};
+		System.out.println(binarySearch(array, 0));
 
+	}
+
+	public static int binarySearch(int[] array, int item) {
+
+		int low = 0;
+		int high = array.length - 1;
+
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			int guess = array[mid];
+			if (guess == item)
+				return mid;
+			else if (guess < item)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		return -1;
 	}
 
 }
